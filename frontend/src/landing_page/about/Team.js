468b3dedccc2eaf17d79826511f2d83f767bd9f1
Akash Mishra <akashmishra967033@gmail.com>
2026-03-21 19:51:35 +0530

    initial changes

--- frontend/src/landing_page/about/Team.js
+++ frontend/src/landing_page/about/Team.js
@@ -8,30 +8,23 @@ function Team() {
             </div>
             <div className='row p-3 text-muted' style={{lineHeight:"1.8", fontSize:"1.2rem"}}>
                
-               <div className='col-12 col-md-6 p-3 text-center'>  {/* ✅ mobile pe full width */}
+               <div className='col-12 col-md-6 p-3 text-center'>
                  <img 
                    src='media/images/nithinKamath.jpg' 
-                   style={{borderRadius:"100%", width:"50%", maxWidth:"200px"}}  {/* ✅ maxWidth fix */}
+                   style={{borderRadius:"100%", width:"50%", maxWidth:"200px"}}
                    alt="Nithin Kamath"
                  />
                  <h4 className='mt-4'>Nithin Kamath</h4>
                  <h6>Founder, CEO</h6>
                </div> 
 
-               <div className='col-12 col-md-6 p-3'>  {/* ✅ mobile pe full width */}
-                 <p>
-                   Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.
-                 </p>
-                 <p>
-                   He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).
-                 </p>
-                 <p>
-                   Playing basketball is his zen.
-                 </p>
-                 <p>
-                   Connect on <a href='#'>Homepage</a> / <a href='#'>TradingQnA</a> / <a href='#'>Twitter</a>
-                 </p>
+               <div className='col-12 col-md-6 p-3'>
+                 <p>Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry.</p>
+                 <p>He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).</p>
+                 <p>Playing basketball is his zen.</p>
+                 <p>Connect on <a href='#'>Homepage</a> / <a href='#'>TradingQnA</a> / <a href='#'>Twitter</a></p>
                </div> 
+
             </div>
         </div>
      );
