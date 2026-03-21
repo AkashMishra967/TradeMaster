0f8c74b83e8dc097767e237e686354e7639ddf7b
Akash Mishra <akashmishra967033@gmail.com>
2026-03-21 18:05:13 +0530

    updated backend

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const session = require("express-session");
-const MongoStore = require("connect-mongo"); // ✅ ADD THIS
+const MongoStore = require("connect-mongo").default; // ✅ ADD THIS
 
 const authRoutes = require("./routes/authRoutes");
 const dataRoutes = require("./routes/dataRoutes");
